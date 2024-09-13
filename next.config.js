032f45b47767a0bfda1a55dd7d44aa3899b930a2
Omphile Morwane <omphilemorwane@gmail.com>
2024-09-13 22:48:45 +0200

    Added next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+        images: {
+          domains: ['cdn.dummyjson.com'], // Add your external image domain here
+        },
+      };
+      
+      module.exports = nextConfig;
