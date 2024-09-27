b4bc817dd6939aa92886a1652a9d02a717b1d3ed
Omphile Morwane <omphilemorwane@gmail.com>
2024-09-27 08:45:30 +0200

    Turned Logo into a home link

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -10,21 +10,26 @@ import Link from "next/link";
  * @returns {JSX.Element} The rendered navigation bar component.
  */
 const Navbar = () => {
-  return (  
+  return (
     <nav className="bg-green-600 text-white py-1 shadow-lg fixed w-full top-0 left-0 z-50">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <div className="h-10">
-            <img
-              src="/images/logo (2).png"
-              alt="Logo"
-              className="h-full w-auto transform scale-150"
-            />
-          </div>
+          <Link href="/">
+            <div className="h-10 cursor-pointer">
+              <img
+                src="/images/logo (2).png"
+                alt="Logo"
+                className="h-full w-auto transform scale-150"
+              />
+            </div>
+            
+          </Link>
+          <Link href="/">
           <h1 className="text-white text-2xl font-bold">EliteFinds</h1>
+          </Link>
         </div>
         <div className="space-x-4">
-          <Link href="/products">
+          <Link href="/">
             <span className="hover:text-gray-300 cursor-pointer">Products</span>
           </Link>
           <Link href="/about">
@@ -40,4 +45,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
